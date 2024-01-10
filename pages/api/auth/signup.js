8f86160d9969ccee74158d57d5c91cee046a299f
njaci1 <githatukelvin@gmail.com>
2024-01-10 21:53:07 +0300

    separating the creation of user and update of network to different api routes

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -37,20 +37,20 @@ async function handler(req, res) {
     return;
   }
   try {
-    // Find the inviter by invite code, or default to the seed user if not found
-    let inviter;
+    // // Find the inviter by invite code, or default to the seed user if not found
+    // let inviter;
 
-    if (inviteCode) {
-      console.log(inviteCode);
-      inviter = await User.findOne({ inviteCode: inviteCode });
-    }
+    // if (inviteCode) {
+    //   console.log(inviteCode);
+    //   inviter = await User.findOne({ inviteCode: inviteCode });
+    // }
 
-    if (!inviter) {
-      console.log('inviter not found');
-      // The invite code could be invalid or missing, so we default to the seed user
-      // Replace 'seedUserInviteCode' with the actual invite code of your seed user
-      inviter = await User.findOne({ inviteCode: 'TD5tkLcdE' });
-    }
+    // if (!inviter) {
+    //   console.log('inviter not found');
+    //   // The invite code could be invalid or missing, so we default to the seed user
+    //   // Replace 'seedUserInviteCode' with the actual invite code of your seed user
+    //   inviter = await User.findOne({ inviteCode: 'TD5tkLcdE' });
+    // }
 
     const newUser = new User({
       username: username,
@@ -65,27 +65,27 @@ async function handler(req, res) {
 
     console.log('user created successfully');
 
-    // Update the invitees arrays of the inviter and its ancestors
-    inviter.inviteesLevel1.push(user._id);
-    await inviter.save();
+    // // Update the invitees arrays of the inviter and its ancestors
+    // inviter.inviteesLevel1.push(user._id);
+    // await inviter.save();
 
-    // Check if the inviter is not the seed user before updating ancestors
-    if (inviter.inviteCode !== 'TD5tkLcdE') {
-      console.log('inviter is not seed user');
-      console.log(inviter.inviter);
-      if (inviter.inviter) {
-        const inviterL2 = await User.findById(inviter.inviter);
-        inviterL2.inviteesLevel2.push(user._id);
-        await inviterL2.save();
+    // // Check if the inviter is not the seed user before updating ancestors
+    // if (inviter.inviteCode !== 'TD5tkLcdE') {
+    //   console.log('inviter is not seed user');
+    //   console.log(inviter.inviter);
+    //   if (inviter.inviter) {
+    //     const inviterL2 = await User.findById(inviter.inviter);
+    //     inviterL2.inviteesLevel2.push(user._id);
+    //     await inviterL2.save();
 
-        if (inviterL2.inviter) {
-          console.log('going to level 3');
-          let inviterL3 = await User.findById(inviterL2.inviter);
-          inviterL3.inviteesLevel3.push(user._id);
-          await inviterL3.save();
-        }
-      }
-    }
+    //     if (inviterL2.inviter) {
+    //       console.log('going to level 3');
+    //       let inviterL3 = await User.findById(inviterL2.inviter);
+    //       inviterL3.inviteesLevel3.push(user._id);
+    //       await inviterL3.save();
+    //     }
+    //   }
+    // }
 
     await db.disconnect();
     res.status(201).send({
@@ -96,6 +96,7 @@ async function handler(req, res) {
       level1: user.inviteesLevel1,
       level2: user.inviteesLevel2,
       level3: user.inviteesLevel3,
+      status: user.registrationStatus,
     });
   } catch (error) {
     console.log(error);
