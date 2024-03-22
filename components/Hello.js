b0ceeb2d3b7215e20b7804992d258d5d7b44295e
njaci1 <githatukelvin@gmail.com>
2024-03-23 01:55:47 +0300

    format amount into currency

--- components/Hello.js
+++ components/Hello.js
@@ -1,11 +1,32 @@
 import Layout from './Layout';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 // components/Hello.js
 export default function Hello() {
   const router = useRouter();
   const { redirect } = router.query;
+
+  const [jackpots, setJackpots] = useState({
+    joinersTotal: 0,
+    weeklyTotal: 0,
+    monthlyTotal: 0,
+    annualTotal: 0,
+  });
+
+  useEffect(() => {
+    fetch('/api/jackpotTotals')
+      .then((response) => response.json())
+      .then((data) => {
+        setJackpots({
+          joinersTotal: data.joinersTotal,
+          weeklyTotal: data.weeklyTotal,
+          monthlyTotal: data.monthlyTotal,
+          annualTotal: data.annualTotal,
+        });
+      });
+  }, []);
   return (
     <Layout title="Hello Page">
       <div className="font-sans min-h-screen py-2">
@@ -14,38 +35,36 @@ export default function Hello() {
             Watch Ads, stand a chance to Win Jackpots!
           </h1>
           <p className="text-lg">
-            Watch ads for a chance to win this week&apos;s jackpot of 50K.
+            Watch ads for a chance to win this week&apos;s jackpot of {'KSH '}
+            {jackpots.weeklyTotal}.
           </p>
           <Link
             href={`/register?inviteCode=&redirect=${redirect || '/'}`}
             className="mt-2 inline-block px-5 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-600"
           >
-            Sign Up Now!
+            Join Now!
           </Link>
         </header>
         <main className="p-5">
           <section className="jackpots flex justify-between mb-5">
-            {['Welcome', 'Weekly', 'Monthly', 'Annual'].map((jackpot) => (
+            {['Joiners', 'Weekly', 'Monthly', 'Annual'].map((jackpot) => (
               <div
                 key={jackpot}
                 className="jackpot text-center p-2 border border-gray-300 rounded"
               >
                 <h3 className="text-lg font-bold mb-1">{jackpot} Jackpot</h3>
                 <p className="text-base">
-                  $<span id={`${jackpot.toLowerCase()}-jackpot`}>100</span>
-                </p>
-
-                <p className="text-base">
-                  Next Draw:{' '}
-                  <span id={`${jackpot.toLowerCase()}-draw-date`}>
-                    2024-03-02
+                  KSH
+                  <span id={`${jackpot.toLowerCase()}-jackpot`}>
+                    {jackpots[`${jackpot.toLowerCase()}Total`]}
                   </span>
                 </p>
+
                 <Link
                   href={`/register?inviteCode=&redirect=${redirect || '/'}`}
                   className="mt-2 inline-block px-5 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-600"
                 >
-                  Sign Up Now!
+                  Join Now!
                 </Link>
               </div>
             ))}
