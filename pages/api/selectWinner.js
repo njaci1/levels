ad2fa6929259f8ac1341ae7b3863f631f74596b0
njaci1 <githatukelvin@gmail.com>
2024-08-25 18:04:29 +0300

    fix issue with distribution of winning to the winner's network

--- pages/api/selectWinner.js
+++ pages/api/selectWinner.js
@@ -1,7 +1,7 @@
 import runJackpot from '../../lib/runJackpot';
 
 export default async function handler(req, res) {
-  const winner = runJackpot('monthly');
+  const winner = runJackpot('weekly');
 
   res.status(200).json(winner);
 }
