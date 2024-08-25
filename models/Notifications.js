28ab1a09888bfb1b7ec3fb664975ecbc438e2b6e
njaci1 <githatukelvin@gmail.com>
2024-08-25 19:35:47 +0300

    create notifications model

---
+++ models/Notifications.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  message: String,
+  read: { type: Boolean, default: false },
+  win: { type: Boolean, default: false },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Notification =
+  mongoose.models.Notification ||
+  mongoose.model('Notification', notificationSchema);
+
+export default Notification;
