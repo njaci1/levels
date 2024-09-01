89d26c911f9017483086fb4f10de8d2fd0d23e85
njaci1 <githatukelvin@gmail.com>
2024-09-01 11:50:23 +0300

    add moment package and use it to get jackpots

--- pages/api/getJackpotEntries.js
+++ pages/api/getJackpotEntries.js
@@ -1,5 +1,6 @@
 import db from '../../lib/db';
 import { getSession } from 'next-auth/react';
+import moment from 'moment';
 import {
   WeeklyJackpotEntry,
   MonthlyJackpotEntry,
@@ -14,16 +15,38 @@ export default async function handler(req, res) {
   }
   await db.connect();
 
+  // Set Thursday as the start of the week
+  moment.updateLocale('en', {
+    week: {
+      dow: 4,
+    },
+  });
+
+  // Get the start and end of the current week
+  const startOfWeek = moment().startOf('week').toDate();
+  const endOfWeek = moment().endOf('week').toDate();
+
+  // Get the start and end of the current month
+  const startOfMonth = moment().startOf('month').toDate();
+  const endOfMonth = moment().endOf('month').toDate();
+
+  // Get the start and end of the current year
+  const startOfYear = moment().startOf('year').toDate();
+  const endOfYear = moment().endOf('year').toDate();
+
   // fetch and count jackpot entries per jackpot
 
   const weeklyEntries = await WeeklyJackpotEntry.countDocuments({
     userId: session.user._id,
+    timestamp: { $gte: startOfWeek, $lte: endOfWeek },
   });
   const monthlyEntries = await MonthlyJackpotEntry.countDocuments({
     userId: session.user._id,
+    timestamp: { $gte: startOfMonth, $lte: endOfMonth },
   });
   const annualEntries = await AnnualJackpotEntry.countDocuments({
     userId: session.user._id,
+    timestamp: { $gte: startOfYear, $lte: endOfYear },
   });
 
   res.status(200).json({ weeklyEntries, monthlyEntries, annualEntries });
