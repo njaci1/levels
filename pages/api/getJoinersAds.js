35f8082de9db546c9c51c1e3a3765706f39bf258
njaci1 <githatukelvin@gmail.com>
2024-09-01 11:56:37 +0300

    move db connection and disconnection to try and catch blocks

--- pages/api/getJoinersAds.js
+++ pages/api/getJoinersAds.js
@@ -3,18 +3,20 @@ import Ad from '../../models/AdsCollection'; // Import the Ad model
 
 export default async function handler(req, res) {
   // Connect to the database
-  await db.connect();
 
   try {
+    await db.connect();
     // Query the database for ads with a rating of 1
     const ads = await Ad.find({ priority: 7 }).select('videoUrl').limit(3);
 
     // Return the ads
     res.status(200).json(ads);
+    await db.disconnect();
   } catch (error) {
     // Handle any errors
+    await db.disconnect();
     console.error(error);
     res.status(500).json({ message: 'Failed to get joiners ads' });
   }
-  await db.disconnect();
+  // await db.disconnect();
 }
