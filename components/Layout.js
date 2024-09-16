c4148cf08f3c0c8aeba701a0e9ff12dcdbe47b67
njaci1 <githatukelvin@gmail.com>
2024-09-16 13:12:56 +0300

    Make the Layout Header sticky on scroll

--- components/Layout.js
+++ components/Layout.js
@@ -42,7 +42,14 @@ export default function Layout({ title, children }) {
       </Head>
 
       <div className="flex min-h-screen flex-col justify-between">
-        <header>
+        <header
+          style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 1000,
+            background: '#fff',
+          }}
+        >
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link className="text-lg font-bold" href="/">
               Home
@@ -86,9 +93,7 @@ export default function Layout({ title, children }) {
             )}
           </nav>
         </header>
-        <main className="container mx-auto px-4 flex-grow overflow-y-auto">
-          {children}
-        </main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
           <p>Copyright © </p>
         </footer>
