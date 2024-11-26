2a856d743440c1795c83d5ebfe1657b254509cf6
njaci1 <githatukelvin@gmail.com>
2024-11-26 17:38:02 +0300

    return success if ad interaction is a first time interaction

--- pages/api/getInteraction.js
+++ pages/api/getInteraction.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       if (interaction) {
         res.status(200).json(interaction); // Return the interaction
       } else {
-        res.status(404).json({ message: 'No interaction found' });
+        res.status(201).json('This is a new interaction');
       }
     } catch (error) {
       console.error('Error fetching interaction:', error);
