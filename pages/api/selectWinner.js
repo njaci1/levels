a5112cdd9e7da57f5a18dcd338822e350382a2c7
njaci1 <githatukelvin@gmail.com>
2024-11-27 14:20:23 +0300

    fix commission distribution issues

--- pages/api/selectWinner.js
+++ pages/api/selectWinner.js
@@ -2,7 +2,7 @@ import runJackpot from '../../lib/runJackpot';
 
 export default async function handler(req, res) {
   try {
-    const winner = await runJackpot('weekly');
+    const winner = await runJackpot('welcome');
     res.status(200).json(winner);
   } catch (error) {
     console.error('Error running jackpot:', error);
