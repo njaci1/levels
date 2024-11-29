82b99ce460930fba4fcad20f77cc7b5e7571d607
njaci1 <githatukelvin@gmail.com>
2024-11-29 13:12:10 +0300

    fix bug on payouts model

--- models/payoutsTotal.js
+++ models/payoutsTotal.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const PayoutsTotalSchema = new mongoose.Schema({
+const payoutsTotalSchema = new mongoose.Schema({
   totalPayouts: { type: Number, default: 0 }, // Overall revenue from ads
 });
 
 const payoutsTotal =
-  mongoose.models.PayoutsTotal ||
-  mongoose.model('payoutsTotal', PayoutsTotalSchema);
+  mongoose.models.payoutsTotal ||
+  mongoose.model('payoutsTotal', payoutsTotalSchema);
 
 export default payoutsTotal;
