195cf21f001c770113800ecb81acef948d72907f
njaci1 <githatukelvin@gmail.com>
2024-12-02 11:40:53 +0300

    update type to jackpotType in the jackpotEntry schema model

--- models/JackpotEntry.js
+++ models/JackpotEntry.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const jackpotEntrySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   timestamp: { type: Date, default: Date.now },
-  type: {
+  jackpotType: {
     type: String,
     enum: ['daily', 'weekly', 'monthly', 'annual', 'welcome'],
     required: true,
   },
 });
 
-jackpotEntrySchema.index({ userId: 1, type: 1, timestamp: 1 });
+jackpotEntrySchema.index({ userId: 1, jackpotType: 1, timestamp: 1 });
 
 const JackpotEntry =
   mongoose.models.JackpotEntry ||
